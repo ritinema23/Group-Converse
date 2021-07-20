eeb838ba5f492190796a1895f00324a8654713ca
Riti Nema <ritinema23@gmail.com>
2021-07-20 19:22:23 +0530

    changed firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCqMnRghkYWfAvCsx8rQBJxtmBE9If6BLk",
-    authDomain: "whats-app-clone-9ec0e.firebaseapp.com",
-    databaseURL: "https://whats-app-clone-9ec0e.firebaseio.com",
-    projectId: "whats-app-clone-9ec0e",
-    storageBucket: "whats-app-clone-9ec0e.appspot.com",
-    messagingSenderId: "252968078305",
-    appId: "1:252968078305:web:afa636ed456a8a64b3e0a0",
-    measurementId: "G-RT24G1VBWY"
-  };
+  apiKey: "AIzaSyDjwZFvLfqOoOllbzlScRCGKl5cnIxfSYM",
+  authDomain: "group-converse.firebaseapp.com",
+  projectId: "group-converse",
+  storageBucket: "group-converse.appspot.com",
+  messagingSenderId: "345197216956",
+  appId: "1:345197216956:web:1fdb91a263d3997770fcb9",
+  measurementId: "G-KLTZMWZTSL"
+};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
